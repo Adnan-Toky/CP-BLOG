4c12d0a8e4cab173f1de6aabc805b5f17c9d75fa
Adnan Toky <adnanzawad@yahoo.com>
2021-07-26 01:45:43 +0600

    x-03

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,8 @@
 import { hexToRgb, makeStyles } from "@material-ui/core/styles";
 import { NavLink } from "react-router-dom";
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { useState } from "react";
 
 function hex2rgb(hex, opacity) {
     if (hex.length == 4) return "rgba(" + ('0x' + hex[1] + hex[1] | 0) + ", " + ('0x' + hex[2] + hex[2] | 0) + ", " + ('0x' + hex[3] + hex[3] | 0) + ", " + opacity + ")";
@@ -7,6 +10,12 @@ function hex2rgb(hex, opacity) {
 }
 
 export default function Header(props) {
+    const [open, setOpen] = useState(0);
+
+    function toggleMenu() {
+        setOpen(!open);
+    }
+
     let fontColor = (props.theme === "default") ? "red" : ((props.theme.custom) ? props.theme.customFontColor : (props.theme.invert) ? props.theme.backgroundColor : props.theme.fontColor);
     let backgroundColor = (props.theme === "default") ? "#444" : ((props.theme.custom) ? props.theme.customBackgroundColor : (props.theme.invert) ? props.theme.fontColor : props.theme.backgroundColor);
     let height = props.ui.width > 720 ? 60 : 45;
@@ -15,7 +24,7 @@ export default function Header(props) {
     const useStyles = makeStyles({
         root: {
             color: fontColor,
-            backgroundColor: hex2rgb(backgroundColor, 0.9),
+            backgroundColor: hex2rgb(backgroundColor, open && props.ui.width <= 720 ? 1 : 0.9),
             // color: backgroundColor,
             color: fontColor,
             // backgroundColor: backgroundColor,
@@ -64,9 +73,41 @@ export default function Header(props) {
             "& li": {
                 borderBottom: "3px solid #0080ff"
             }
+        },
+        menuBtn: {
+            position: "absolute",
+            right: 15,
+            top: 10,
+            visibility: props.ui.width > 720 ? "hidden" : "visible"
+        },
+        mobileMenu: {
+            backgroundColor: hex2rgb(backgroundColor, 0.99),
+            height: props.ui.height - 45,
+            position: "absolute",
+            width: "100%"
+        },
+        activeLinkMobile: {
+            color: "#0080ff"
+        },
+        mobileNav: {
+            listStyleType: "none",
+            margin: 0,
+            padding: 0,
+            marginTop: 30,
+            "& li": {
+                height: 50,
+                color: fontColor,
+                marginLeft: 0,
+                paddingLeft: 0,
+                textAlign: "center",
+                width: "100%",
+                fontSize: 20,
+                fontWeight: 700
+            }
         }
     });
     const classes = useStyles();
+
 
     return (
         <div className={classes.root}>
@@ -77,6 +118,16 @@ export default function Header(props) {
                 <NavLink activeClassName={classes.activeLink} to="/posts"><li>POSTS</li></NavLink>
                 <NavLink activeClassName={classes.activeLink} to="/about"><li>ABOUT</li></NavLink>
             </ul>
+            {open ? <CloseIcon className={classes.menuBtn} onClick={toggleMenu} /> : <MenuIcon className={classes.menuBtn} onClick={toggleMenu} />}
+            <div className={classes.mobileMenu} style={{
+                display: open && props.ui.width <= 720 ? "block" : "none"
+            }}>
+                <ul className={classes.mobileNav}>
+                    <NavLink activeClassName={classes.activeLinkMobile} style={{ textDecoration: "none" }} exact to="/"><li>HOME</li></NavLink>
+                    <NavLink activeClassName={classes.activeLinkMobile} style={{ textDecoration: "none" }} to="/posts"><li>POSTS</li></NavLink>
+                    <NavLink activeClassName={classes.activeLinkMobile} style={{ textDecoration: "none" }} to="/about"><li>ABOUT</li></NavLink>
+                </ul>
+            </div>
         </div>
     );
 }
